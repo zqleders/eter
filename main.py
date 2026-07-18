f8ae8460c46a5bd830ac45b4104a46018354687d
zqleaders <zqleaders@gmail.com>
2026-07-18 17:21:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -191,8 +191,11 @@ def run_automation():
                         time.sleep(10)
 
                 else:
-
-                    print("[LOG] 未发现人机验证框")
+                        page.goto(f"{BASE_URL}/servers/5541/info")
+                        page.wait_for_load_state("domcontentloaded")
+                        time.sleep(5)
+                        force_remove_and_disable_ads(page)
+                        print("[LOG] 未发现人机验证框")
 
                     
 
